b3c7f749d847b42cda2d7d22fc4198145591f5d5
orelljon <orell890@gmail.com>
2024-06-28 16:06:02 +0300

    Update datanhaku.py

--- datanhaku.py
+++ datanhaku.py
@@ -25,3 +25,9 @@ def get_ot_lkm_ol():
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
         df = pd.read_sql(query, conn)
     return df
+    
+def get_ta_lkm():
+    query = "SELECT toimeksiantaja FROM theseusAMK;"
+    with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
+        df = pd.read_sql(query, conn)
+    return df
