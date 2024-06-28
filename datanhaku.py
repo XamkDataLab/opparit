a533f284f052523491e80a8eb052c91300d0a1e7
Xamk DataLab <123467724+XamkDataLab@users.noreply.github.com>
2024-06-28 09:32:34 +0300

    Create datanhaku.py

---
+++ datanhaku.py
@@ -0,0 +1,21 @@
+import pyodbc
+import streamlit as st
+import pandas as pd
+
+driver = st.secrets["driver"]
+server = st.secrets["server"]
+database = st.secrets["database"]
+username = st.secrets["username"]
+password = st.secrets["password"]
+
+def get_theseus_data():
+    query = "SELECT id, julkaisupäivä, oppilaitos FROM theseusAMK;"
+    with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
+        df = pd.read_sql(query, conn)
+    return df
+
+def get_theseus_data_keywords():
+    query = "SELECT id, julkaisupäivä, avainsanat FROM theseusAMK;"
+    with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
+        df2 = pd.read_sql(query, conn)
+    return df2
