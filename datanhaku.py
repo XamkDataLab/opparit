f044d26f31a89c3cfb565fccb77f51580ca0c855
orelljon <orell890@gmail.com>
2024-06-28 16:07:22 +0300

    Update datanhaku.py

--- datanhaku.py
+++ datanhaku.py
@@ -20,8 +20,8 @@ def get_theseus_data_keywords():
         df = pd.read_sql(query, conn)
     return df
 
-def get_ot_lkm_ol():
-    query = "SELECT oppilaitos, id FROM theseusAMK;"
+def get_ta_lkm():
+    query = "SELECT toimeksiantaja FROM theseusAMK;"
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
         df = pd.read_sql(query, conn)
     return df
