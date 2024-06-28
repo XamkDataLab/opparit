594f3dfa6a738587eb9592b4f3ca07214a6ea18c
orelljon <orell890@gmail.com>
2024-06-28 15:42:22 +0300

    Update äppi.py

--- äppi.py
+++ äppi.py
@@ -18,7 +18,7 @@ df["oppilaitos"] = df["oppilaitos"].replace("Karelia-ammattikorkeakoulu (Pohjois
 st.subheader('Opinnäytetöiden määrä oppilaitoksittain')
 opinnäytetyöt_oppilaitoksittain = df.groupby("oppilaitos")["id"].nunique().reset_index()
 opinnäytetyöt_oppilaitoksittain = opinnäytetyöt_oppilaitoksittain.sort_values(by="id", ascending=False)
-fig, px = plt.subplots(figsize=(12, 7))
+fig, px = plt.subplots(figsize=(10, 5))
 px.bar(opinnäytetyöt_oppilaitoksittain["oppilaitos"], opinnäytetyöt_oppilaitoksittain["id"], color=plt.cm.coolwarm(range(len(opinnäytetyöt_oppilaitoksittain))))
 px.set_xlabel("Oppilaitos", fontsize=15, color='white')
 px.set_ylabel("Opinnäytteiden määrä", fontsize=15, color='white')
