a893093f02d0c323c5d56472bae0525a95a9fbfb
orelljon <orell890@gmail.com>
2024-07-01 13:51:30 +0300

    Update datanhaku.py

--- datanhaku.py
+++ datanhaku.py
@@ -37,4 +37,10 @@ def get_theseus_data_keywords():
     query = "SELECT id, julkaisupäivä, avainsanat FROM theseusAMK;"
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
         df = pd.read_sql(query, conn)
-    return df
+    return df;
+
+def get_kielilkm():
+    query = "SELECT kieli FROM theseusAMK;"
+    with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
+        df = pd.read_sql(query, conn)
+    return df;
