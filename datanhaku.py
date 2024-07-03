6b572ec99b6f315bfd6357b951d9769f32471333
orelljon <orell890@gmail.com>
2024-07-03 11:54:38 +0300

    Update datanhaku.py

--- datanhaku.py
+++ datanhaku.py
@@ -8,7 +8,6 @@ database = st.secrets["database"]
 username = st.secrets["username"]
 password = st.secrets["password"]
 
-
 def get_ta_lkm():
     query = "SELECT toimeksiantaja FROM theseusAMK;"
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
