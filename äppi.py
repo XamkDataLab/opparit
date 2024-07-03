de065ee98d2c936dc996a4e5da27b24305e760fe
orelljon <orell890@gmail.com>
2024-07-03 19:37:02 +0300

    Update äppi.py

--- äppi.py
+++ äppi.py
@@ -9,7 +9,7 @@ import re
 from streamlit_option_menu import option_menu
 from datanhaku import *
 #----------------
-st.title('TheseusAMK visualisointi')
+st.title('💻 TheseusAMK visualisointi')
 valinnat = option_menu(None, ["Toimeksiannot", 'Koulutusohjelmat', 'Muut'], 
     menu_icon="cast", default_index=0, orientation="horizontal",
     styles={
