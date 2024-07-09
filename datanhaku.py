634047403580e7371639b70546cd5a2f0e615aee
orelljon <orell890@gmail.com>
2024-07-09 13:39:38 +0300

    Update datanhaku.py

--- datanhaku.py
+++ datanhaku.py
@@ -164,3 +164,9 @@ def get_vis21():
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
         df = pd.read_sql(query, conn)
     return df
+
+def get_vis22():
+    query = "SELECT vuosi, tiivistelmä1 FROM theseusAMK;"
+    with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
+        df = pd.read_sql(query, conn)
+    return df
