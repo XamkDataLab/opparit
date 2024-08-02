6bf66aa6cc4d734a9ee2dfaec0a52961e5dead63
orelljon <orell890@gmail.com>
2024-08-02 17:40:18 +0300

    Update datanhaku.py

--- datanhaku.py
+++ datanhaku.py
@@ -127,3 +127,9 @@ def get_vis17():
     with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
         df = pd.read_sql(query, conn)
     return df
+
+def get_ccn():
+    query = "SELECT toimeksiantaja FROM theseusAMK;"
+    with pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}') as conn:
+        df = pd.read_sql(query, conn)
+    return df
